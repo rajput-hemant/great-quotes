461a8f48b489787aae377e1bcb95f6c9e0ba1239
rajput-hemant <hemant.rajput_cs20@gla.ac.in>
2022-12-23 21:27:15 +0530

    created use-http hook & QuoteApi for integrating firebase
    Signed-off-by: rajput-hemant <hemant.rajput_cs20@gla.ac.in>

---
+++ src/lib/api.js
@@ -0,0 +1,104 @@
+const FIREBASE_DOMAIN = process.env.REACT_APP_FIREBASE_DOMAIN;
+
+class QuotesAPI {
+	async getAllQuotes() {
+		const response = await fetch(`${FIREBASE_DOMAIN}/quotes.json`);
+		const data = await response.json();
+
+		if (!response.ok) {
+			throw new Error(data.message || "Could not fetch quotes.");
+		}
+
+		const transformedQuotes = [];
+
+		for (const key in data) {
+			const quoteObj = {
+				id: key,
+				...data[key],
+			};
+
+			transformedQuotes.push(quoteObj);
+		}
+
+		return transformedQuotes;
+	}
+
+	async getSingleQuote(quoteId) {
+		const response = await fetch(`${FIREBASE_DOMAIN}/quotes/${quoteId}.json`);
+		const data = await response.json();
+
+		if (!response.ok) {
+			throw new Error(data.message || "Could not fetch quote.");
+		}
+
+		const loadedQuote = {
+			id: quoteId,
+			...data,
+		};
+
+		return loadedQuote;
+	}
+
+	async addQuote(quoteData) {
+		const response = await fetch(`${FIREBASE_DOMAIN}/quotes.json`, {
+			method: "POST",
+			body: JSON.stringify(quoteData),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+		const data = await response.json();
+
+		if (!response.ok) {
+			throw new Error(data.message || "Could not create quote.");
+		}
+
+		return null;
+	}
+
+	async addComment(requestData) {
+		const response = await fetch(
+			`${FIREBASE_DOMAIN}/comments/${requestData.quoteId}.json`,
+			{
+				method: "POST",
+				body: JSON.stringify(requestData.commentData),
+				headers: {
+					"Content-Type": "application/json",
+				},
+			}
+		);
+		const data = await response.json();
+
+		if (!response.ok) {
+			throw new Error(data.message || "Could not add comment.");
+		}
+
+		return { commentId: data.name };
+	}
+
+	async getAllComments(quoteId) {
+		const response = await fetch(`${FIREBASE_DOMAIN}/comments/${quoteId}.json`);
+
+		const data = await response.json();
+
+		if (!response.ok) {
+			throw new Error(data.message || "Could not get comments.");
+		}
+
+		const transformedComments = [];
+
+		for (const key in data) {
+			const commentObj = {
+				id: key,
+				...data[key],
+			};
+
+			transformedComments.push(commentObj);
+		}
+
+		return transformedComments;
+	}
+}
+
+const api = new QuotesAPI();
+export default api;
